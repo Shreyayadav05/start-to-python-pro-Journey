b66b658b2da0545b8e6a0d0ed1561a65b8b1293e
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2025-11-11 19:11:47 +0530

    Implement number classification based on digits
    Add logic to classify numbers as 'Special' or 'Normal'.

---
+++ printing the score.py
@@ -0,0 +1,15 @@
+#if 'd' is less than 10 we are printing 'd'itself otherwise print (10+(d-10)*3)
+
+n=int(input())
+a= n // 10
+b=n%10
+sum_digits = a+b
+if sum_digits == 7:
+    print("Special Number")
+elif a == 7  or b ==7:
+    print("Special Number")
+elif(n%7==0):
+    print("Special Number")
+else:
+    print("Normal Number")
+
