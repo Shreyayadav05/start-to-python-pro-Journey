9e2ef20b81d5c7ec2f1b2143f33e3963380c91b0
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2025-11-15 15:22:28 +0530

    Create Armstrong number for 4 digits.py

---
+++ Armstrong number for 4 digits.py
@@ -0,0 +1,9 @@
+n=input()
+one=int(n[0])**4
+two=int(n[1])**4
+three=int(n[2])**4
+four=int(n[3])**4
+if(one+two+three+four==int(n)):
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
