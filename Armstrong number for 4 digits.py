df361323e46b71e2566604d9cf0afc5fccef5e68
Shreya B Yadav <149358632+Shreyayadav05@users.noreply.github.com>
2026-03-11 23:37:01 +0530

    Fix print statement case for Armstrong number

--- Armstrong number for 4 digits.py
+++ Armstrong number for 4 digits.py
@@ -4,6 +4,6 @@ two=int(n[1])**4
 three=int(n[2])**4
 four=int(n[3])**4
 if(one+two+three+four==int(n)):
-    print("Armstrong Number")
+    print("armstrong number")
 else:
     print("Not an Armstrong Number")
